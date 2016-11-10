eb2cd775478f816e2b2e1275e382339386acdf93
Yomikoda <yomiko@Yomiko-Padalecki.local>
2016-11-10 17:02:59 +0100

    MAJOR CORRECTIONS

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
-app.get('/page', function(req, res){
-  res.sendFile(__dirname + '/tmpl/html/page.html');
+app.use('/js', express.static('assets/js'));
+app.use('/css', express.static('assets/css'));
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
-http.listen(1337, function(){
-  console.log('listening on *:1337');
+app.get('/test', function(req, res){
+  res.send('<h1>Hello TEST</h1>');
+});
+
+app.get('/page', function(req, res){
+  res.sendFile(__dirname + '/tmpl/page.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
